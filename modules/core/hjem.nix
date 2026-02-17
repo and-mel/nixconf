41e2756a83180a12816111620999366f1312f963
and-mel <amelikhov9836@gmail.com>
2026-02-17 17:25:29 -0500

    temporarily disable hjem

--- modules/core/hjem.nix
+++ modules/core/hjem.nix
@@ -1,14 +1,14 @@
 { inputs, lib, pkgs, user, ... }:
 {
-  hjem.users.${user} = {
-    enable = true;
-    files = {
-      ".ssh/config".text = ''
-        Host github.com
-          User git
-          IdentityFile ~/.ssh/id_ed25519_sk
-          IdentitiesOnly yes
-      '';
-    };
-  };
+  # hjem.users.${user} = {
+  #   enable = true;
+  #   files = {
+  #     ".ssh/config".text = ''
+  #       Host github.com
+  #         User git
+  #         IdentityFile ~/.ssh/id_ed25519_sk
+  #         IdentitiesOnly yes
+  #     '';
+  #   };
+  # };
 }
