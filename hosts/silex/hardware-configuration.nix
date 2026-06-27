ca14c1ebef988f9a953bbe5b2473c7040a160a7b
NixOS Installer <88601482+and-mel@users.noreply.github.com>
2026-06-27 01:51:16 +0000

    [installer] 1782525067: Add hardware configuration for host silex

--- hosts/silex/hardware-configuration.nix
+++ hosts/silex/hardware-configuration.nix
@@ -8,9 +8,9 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
